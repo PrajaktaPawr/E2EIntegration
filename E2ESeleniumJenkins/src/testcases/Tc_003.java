bd48a6762819316af4cac2045d0e7ee0c2037ff8
prajaktapawar-git <prajaktapawar.pp@gmail.com>
2021-08-05 17:35:26 +0530

    Created Automation project

---
+++ E2ESeleniumJenkins/src/testcases/Tc_003.java
@@ -0,0 +1,24 @@
+package testcases;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Tc_003 {
+	
+	@Test
+	public void testcase1()
+	{
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		//Open page
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("prajaktapawar.pp@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("123456789");
+		driver.quit();
+	}
+
+}
